bc2adf7b1ead705acd3eb85c8b87806f4f5ce75c
karanmann <karan.mann@outlook.com>
2022-10-14 13:19:06 +0200

    Main app file structure snad styleing done

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,47 @@
 <template>
-  <router-view/>
+  <header>
+    <router-link to="/">
+      <h1>N<span>(Vue)</span>Movies</h1>
+    </router-link>
+  </header>
+  <main>
+    <router-view />
+  </main>
 </template>
 
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: Avenir, system-ui, Roboto, sans-serif;
 
+  &::selection {
+    background: transparentize(#42b883, 0.5);
+  }
+}
+body {
+  background-color: #35495e;
+}
+
+a {
+  text-decoration: none;
+}
+
+header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px 16px;
+  background-color: #2c3d4e;
+  box-shadow: 0 0 6px rgba(0, 0, 0, 0.1)
+}
+
+h1 {
+  color: white;
+  font-size: 28px;
+  span {
+    color: #42b883
+  }
+}
 </style>
