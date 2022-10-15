0ad78a22804651869bf9b86bd2e0749396bcea63
karanmann <karan.mann@outlook.com>
2022-10-15 06:56:07 +0200

    checkout

--- src/views/MovieDetail.vue
+++ src/views/MovieDetail.vue
@@ -1,11 +1,39 @@
 <template>
-  <div class="movie-detail">Detail {{ $route.params.id }}</div>
+  <div class="movie-detail">
+    <h2 >{{movie.Title}}</h2>
+  </div>
 </template>
 
 <script>
+import { ref, onBeforeMount } from "vue";
+import { useRoute } from "vue-router";
+
 export default {
-    
+  setup() {
+    const movie = ref({});
+    const route = useRoute();
+
+    onBeforeMount(() => {
+      fetch(
+        `http://www.omdbapi.com/?apikey=${process.env.VUE_APP_OMDB_OPEN_API_TOKEN}&i=${route.params.id}&plot=full`
+      )
+        .then(res=> res.json())
+        .then(data => {
+					movie.value = data
+					console.log(movie.value)
+				});
+
+      return {
+        movie,
+      };
+    });
+  },
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+
+.movie-detail {
+	color: white;
+}
+</style>
