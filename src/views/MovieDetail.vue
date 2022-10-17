ea01780e2e9aac797231b875f76c9e4e66512c52
karanmann <karan.mann@outlook.com>
2022-10-17 11:27:12 +0200

    Finished the Movie Detail Page

--- src/views/MovieDetail.vue
+++ src/views/MovieDetail.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="movie-detail">
-    <h2 >{{movie.Title}}</h2>
+    <h2>{{ movie.Title }}</h2>
+    <p>{{ movie.Year }}</p>
+    <img :src="movie.Poster" alt="Movie Poster" class="featured-img" />
+    <p>{{ movie.Plot }}</p>
   </div>
 </template>
 
@@ -12,28 +15,42 @@ export default {
   setup() {
     const movie = ref({});
     const route = useRoute();
-
+    console.log(movie);
     onBeforeMount(() => {
       fetch(
         `http://www.omdbapi.com/?apikey=${process.env.VUE_APP_OMDB_OPEN_API_TOKEN}&i=${route.params.id}&plot=full`
       )
-        .then(res=> res.json())
-        .then(data => {
-					movie.value = data
-					console.log(movie.value)
-				});
-
-      return {
-        movie,
-      };
+        .then((response) => response.json())
+        .then((data) => {
+          movie.value = data;
+          console.log(movie.value)
+        });
     });
+    return {
+      movie,
+    };
   },
 };
 </script>
 
 <style lang="scss">
-
 .movie-detail {
-	color: white;
+  padding: 16px;
+  h2 {
+    color: #fff;
+    font-size: 28px;
+    font-weight: 600;
+    margin-bottom: 16px;
+  }
+  .featured-img {
+    display: block;
+    max-width: 200px;
+    margin-bottom: 16px;
+  }
+  p {
+    color: #fff;
+    font-size: 18px;
+    line-height: 1.4;
+  }
 }
 </style>
